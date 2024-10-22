2795c8f0192a950193040b1c6395d343a93a2f5b
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 17:16:31 +0100

    Implementaçao para calculo do consumo médio de um carro.

--- ConsumoMedioCarro/Program.cs
+++ ConsumoMedioCarro/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsumoMedioCarro
+﻿//Problema "consumo"
+//Fazer um programa para ler a distância total (em km) percorrida por um carro, bem como o total de
+//combustível gasto por este carro ao percorrer tal distância. Seu programa deve mostrar o consumo
+//médio do carro, com três casas decimais. 
+
+using System.Globalization;
+
+namespace ConsumoMedioCarro
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int distanciaPercorrida;
+            double combustivelGasto, consumoMedio;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Distancia percorrida: ");
+            distanciaPercorrida = int.Parse(Console.ReadLine());
+
+            Console.Write("Combustível gasto: ");
+            combustivelGasto = double.Parse(Console.ReadLine(), CI);
+
+            consumoMedio = distanciaPercorrida / combustivelGasto;
+
+            Console.Write($"Consumo medio = {consumoMedio.ToString("F3", CI)}");
         }
     }
 }
